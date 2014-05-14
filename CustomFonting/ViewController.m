65f89297bdac5daddb0ec3fa7e2d1e2f2ba8dc1b
Mohit Sadhu <msadhu@msadhu.local>
2014-05-14 17:14:59 -0400

    Adding icon fonts and using them.

--- CustomFonting/ViewController.m
+++ CustomFonting/ViewController.m
@@ -13,11 +13,26 @@
 @end
 
 @implementation ViewController
+{
+    UIFont *customFont;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //customFont = [UIFont fontWithName:@"fontawesome-webfont" size:20.0f];
+    //customFont = [UIFont fontWithName:@"fontello" size:400.0f];
+    customFont = [UIFont fontWithName:@"COA-font-icon" size:40.0f];
+    
+    self.lblFont.font = customFont;
+    self.lblFont.textAlignment = NSTextAlignmentCenter;
+    [self.lblFont setText:[NSString stringWithUTF8String:"\uE600"]];
+    
+    self.lblFont.textColor = [UIColor orangeColor];
+    //self.lblFont.shadowColor = [UIColor blackColor];
+    //self.lblFont.shadowOffset = CGSizeMake(1.0f, 1.0f);
+    
 }
 
 - (void)didReceiveMemoryWarning
