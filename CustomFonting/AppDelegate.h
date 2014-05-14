92386cf00a435e1700e77f3e90784ad5ba5057e7
Mohit Sadhu <msadhu@msadhu.local>
2014-05-14 10:02:55 -0400

    Initial Commit

---
+++ CustomFonting/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomFonting
+//
+//  Created by Mohit Sadhu on 5/14/14.
+//  Copyright (c) 2014 Fareportal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomFonting/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CustomFonting
+//
+//  Created by Mohit Sadhu on 5/14/14.
+//  Copyright (c) 2014 Fareportal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
